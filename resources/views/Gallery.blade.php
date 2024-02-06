5ac6cad83423bbf1c6797617b6d48142d080ea08
unknown <npkinana@gmail.com>
2024-02-06 10:42:59 +0700

    first commit

---
+++ resources/views/Gallery.blade.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head><script src="https://cdn.tailwindcss.com"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+
+<div class="grid grid-cols-2 md:grid-cols-4 gap-4">
+    <div class="grid gap-4">
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src="https://th.bing.com/th/id/OIP.3nMAa6tZ9UhIoclM-fzMkwHaJQ?rs=1&pid=ImgDetMain" alt="">
+        </div>
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src="https://th.bing.com/th/id/R.d7aea93bfc46cb7e7ed858282ea66431?rik=nBaCsDfZdSxP7g&riu=http%3a%2f%2f1.bp.blogspot.com%2f-GV6sZ8sBqvI%2fU6kiPehczXI%2fAAAAAAAAAqs%2fiLnB6v2PNhY%2fs1600%2fprabowo-muda.jpg&ehk=58AuPIY%2bZ4WZnNtYc5dJ3augZae2QxR%2bg1mK918lvL0%3d&risl=&pid=ImgRaw&r=0" alt="">
+        </div>
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src="https://4.bp.blogspot.com/-ZGAGV4AiB70/U46NlKCkQ6I/AAAAAAAAAv4/UYjgVxsKEXc/s1600/bowo.jpg" alt="">
+        </div>
+    </div>
+    <div class="grid gap-4">
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src="https://th.bing.com/th/id/R.35d3ee6a6b0b7ef1085aa548cdf01bc0?rik=utkfbBGl1Hyd7g&riu=http%3a%2f%2f1.bp.blogspot.com%2f-yNsBLaIYEak%2fU32Oy-4SabI%2fAAAAAAAAixc%2fMNb5C68V7Kk%2fs1600%2fPRABOWO%2bMUDA%2b3.png&ehk=DrGhwLt8t5wfWFypTkm8T3tpz0C2TEvnmxDMfHgQPmI%3d&risl=&pid=ImgRaw&r=0" alt="">
+        </div>
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src="https://cdn.idntimes.com/content-images/post/20181017/13532031-558794207624625-556464739-n-a2165edd41beab345dc9d2e23878fdfa.jpg" alt="">
+        </div>
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src="https://alchetron.com/cdn/prabowo-subianto-djojohadikusumo-02e36f62-acf1-479a-a54f-07dbfc44d9d-resize-750.jpg" alt="">
+        </div>
+    </div>
+    <div class="grid gap-4">
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src="https://www.greanvillepost.com/wp-content/uploads/2014/07/indonesia-prabowo-subianto.jpg" alt="">
+        </div>
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src="https://th.bing.com/th/id/OIP.hsN9HWql7jzPcaPL_1g-_wHaJ3?rs=1&pid=ImgDetMain" alt="">
+        </div>
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src="https://fajar.co.id/wp-content/uploads/2021/07/IMG_20210725_111553.jpg" alt="">
+        </div>
+    </div>
+    <div class="grid gap-4">
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src="https://www.scmp.com/sites/default/files/2014/03/24/cb29ecc3e9a18fee2982272faa835e8d.jpg" alt="">
+        </div>
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src="https://th.bing.com/th/id/OIP.CYTJpddXdJjyZBXpJQLVyAHaNJ?rs=1&pid=ImgDetMain" alt="">
+        </div>
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src="https://pict-a.sindonews.net/dyn/620/pena/news/2021/03/06/14/356302/prabowo-pasang-foto-muda-dan-ucapkan-dirgahayu-kostrad-warganet-keren-pak-jenderal-ybk.jpg" alt="">
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
